31dc3f99925b33f38a19ce74f48ceb45da8aab4c
Oleg Sikorskiy <olegsik@gmail.com>
2016-09-18 15:13:16 +0300

    Добавлен интерфейс репозитория.

---
+++ Clusters/ClusterDomain/DataSetRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ClusterDomain
+{
+    public interface DataSetRepository
+    {
+        IEnumerable<DataSet> GetAllDataSets();
+        DataSet GetDataSetByName(string name);
+        void SaveDataSet(DataSet dataSet);
+        void DeleteDataSet(string name);
+    }
+}
