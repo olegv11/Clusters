c54e718d3260043a6eee457358d4f417a6488ef5
Oleg Sikorskiy <olegsik@gmail.com>
2016-09-20 22:35:09 +0300

    Добавил тесты для DataSet

---
+++ Clusters/ClusterDomainTest/DataSetTest.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ClusterDomain;
+using FakeItEasy;
+using FluentAssertions;
+using Xunit;
+
+namespace ClusterDomainTest
+{
+    public class DataSetTest
+    {
+        [Fact]
+        public void DataSetShouldBeEmptyOnDefaulyConstructor()
+        {
+            // Arrange
+            var dataSet = new DataSet();
+
+            // Assert
+            dataSet.Data.Should().BeEmpty("по умолчанию создаётся пустое множество данных");
+        }
+
+        [Fact]
+        public void DataSetShouldContainAllElementsFromEnumerableSuppliedToConstructor()
+        {
+            // Arrange
+            IEnumerable<DataPoint> enumerable = new List<DataPoint>
+            {
+                A.Fake<DataPoint>(),
+                A.Fake<DataPoint>(),
+                A.Fake<DataPoint>(),
+            };
+
+            // Act
+            var dataSet = new DataSet(enumerable);
+
+            // Assert
+            dataSet.Data.Should().BeEquivalentTo(enumerable, 
+                "при создании множества с помощью IEnumerable оно должно содержать все его элементы");
+        }
+
+        [Fact]
+        public void DataSetShouldAddElement()
+        {
+            // Arrange
+            var point = A.Fake<DataPoint>();
+            var dataSet = new DataSet();
+
+            // Act
+            dataSet.AddPoint(point);
+
+            // Assert
+            dataSet.Data.Should().HaveCount(1).And
+                .Contain(point, "при добавлении точки она должна содержаться в множестве");
+        }
+
+        [Fact]
+        public void DataSetShouldAddMultipleElements()
+        {
+            // Arrange
+            IEnumerable<DataPoint> enumerable = new List<DataPoint>
+            {
+                A.Fake<DataPoint>(),
+                A.Fake<DataPoint>(),
+                A.Fake<DataPoint>(),
+            };
+            var dataSet = new DataSet();
+
+            // Act
+            dataSet.AddPoints(enumerable);
+
+            // Assert
+            dataSet.Data.Should().BeEquivalentTo(enumerable);
+        }
+
+        [Fact]
+        public void DataSetShouldDetermineIfItHasElement()
+        {
+            // Arrange
+            var pointIn = A.Fake<DataPoint>();
+            var pointOut = A.Fake<DataPoint>();
+            var dataSet = new DataSet();
+
+            // Act
+            dataSet.AddPoint(pointIn);
+
+            // Assert
+            dataSet.HasPoint(pointIn).Should().BeTrue();
+            dataSet.HasPoint(pointOut).Should().BeFalse();
+        }
+    }
+}
