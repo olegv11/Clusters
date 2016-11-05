fc671e4703c7116ed0cc109ff70f70b6cc6dfb72
Oleg Sikorskiy <olegsik@gmail.com>
2016-11-05 21:30:44 +0300

    Добавил проект для приёмочного тестирования

---
+++ Clusters/AcceptanceTests/Clusterization.feature
@@ -0,0 +1,14 @@
+﻿Feature: Clusterization
+	Кластеризация точек, введённых на графике
+
+Scenario Outline: Succesful Clusterization
+	Given I entered points <points>
+	And I entered minimal number of points <numPoints>
+	And I entered epsilon <epsilon>
+	And I chose metric <metric>
+	When I pressed clusterize
+	Then There must be clusters <clusters> and noise <noise>
+Examples: 
+| points                                                                              | numPoints | epsilon | metric | clusters                                                          | noise                  |
+| {(0;0),(1;0),(0.5;0.1),(-1;-0.2),(10;10),(10.2;9.9),(11.1;10), (100;100); (-10;10)} | 2         | 2       | LP(2)  | {(0;0),(1;0),(0.5;0.1),(-1;-0.2)}, {(10;10),(10.2;9.9),(11.1;10)} | {(100;100); (-10;10)}  |
+| {(1;1)}, (5;5), (7;7)}                                                              | 2         | 1       | Sup    | {}                                                                | {(1;1)}, (5;5), (7;7)} |
---
+++ Clusters/AcceptanceTests/ClusterizationSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace AcceptanceTests
+{
+    [Binding]
+    public class ClusterizationSteps
+    {
+        [Given(@"I entered points \{\((.*)\),\((.*)\),\((.*);(.*)\),\((.*);(.*)\),\((.*)\),\((.*);(.*)\),\((.*);(.*)\), \((.*)\); \((.*)\)}")]
+        public void GivenIEnteredPoints(string p0, string p1, Decimal p2, Decimal p3, int p4, Decimal p5, string p6, Decimal p7, Decimal p8, Decimal p9, int p10, string p11, string p12)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I entered minimal number of points (.*)")]
+        public void GivenIEnteredMinimalNumberOfPoints(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I entered epsilon (.*)")]
+        public void GivenIEnteredEpsilon(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I chose metric LP\((.*)\)")]
+        public void GivenIChoseMetricLP(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I pressed clusterize")]
+        public void WhenIPressedClusterize()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"There must be clusters \{\((.*)\),\((.*)\),\((.*);(.*)\),\((.*);(.*)\)}, \{\((.*)\),\((.*);(.*)\),\((.*);(.*)\)} and noise \{\((.*)\); \((.*)\)}")]
+        public void ThenThereMustBeClustersAndNoise(string p0, string p1, Decimal p2, Decimal p3, int p4, Decimal p5, string p6, Decimal p7, Decimal p8, Decimal p9, int p10, string p11, string p12)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
